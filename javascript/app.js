288a0551523c8ced326a10424e35bbb4a5e3352e
dadaxanov ubaydulloh <lazizbekxoljigitov@gmail.com>
2026-03-22 16:11:20 +0500

    projact-2.1

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,28 @@
 const bars = document.querySelector("#bars__icon");
 const menu = document.querySelector(".bars__main");
+const audio = document.querySelector(".audio");
+const playe = document.querySelector("#playe");
+const pauses = document.querySelector("#pausee");
+
+function playew() {
+  if ((playe.style.display = "none")) {
+    audio.play();
+    pauses.style.display = "inline";
+  } else {
+    audio.pause();
+    playe.style.display = "inline";
+  }
+}
+
+function pause() {
+  if ((pauses.style.display = "none")) {
+    audio.pause();
+    playe.style.display = "inline";
+  } else {
+    audio.play();
+    pauses.style.display = "inline";
+  }
+}
 
 function menuAll() {
   if (menu.style.display == "none") {
